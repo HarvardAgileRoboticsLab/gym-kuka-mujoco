53cc194fabd5e64547db3ebc973be60bc6464ab3
Patrick Varin <patrickjvarin@gmail.com>
2018-11-12 14:58:41 -0500

    formatting

--- gym_kuka_mujoco/envs/id_controlled_kuka_env.py
+++ gym_kuka_mujoco/envs/id_controlled_kuka_env.py
@@ -3,6 +3,7 @@ import mujoco_py
 from gym import spaces
 from gym_kuka_mujoco.envs import kuka_env
 
+
 class IdControlledKukaEnv(kuka_env.KukaEnv):
     '''
     A Kuka environment that uses a low-level inverse dynamics controller
@@ -16,7 +17,6 @@ class IdControlledKukaEnv(kuka_env.KukaEnv):
                  kd_pd=None,
                  **kwargs):
 
-
         # Initialize control quantities
         # Note: This must be before the super class constructor because
         #   it calls get_torque() which requires these variables to be set
@@ -29,7 +29,7 @@ class IdControlledKukaEnv(kuka_env.KukaEnv):
 
         super(IdControlledKukaEnv, self).__init__(**kwargs)
 
-        self.frame_skip = 50 # Control at 10 Hz
+        self.frame_skip = 50  # Control at 10 Hz
 
         # Set the controller gains
         # Note: This must be after the super class constructor is called
@@ -42,29 +42,28 @@ class IdControlledKukaEnv(kuka_env.KukaEnv):
                     0*np.minimum(self.kp_pd*self.model.opt.timestep,
                         2*np.sqrt(self.subtree_mass()*self.kp_pd))
 
-
         # Set the action and observation spaces
         # Note: This must be after the super class constructor is called to
         #   overwrite the original action space.
-        low_pos = self.model.jnt_range[:,0]
-        high_pos = self.model.jnt_range[:,1]
+        low_pos = self.model.jnt_range[:, 0]
+        high_pos = self.model.jnt_range[:, 1]
 
-        low_vel = -3*np.ones(self.model.nv)
-        high_vel = 3*np.ones(self.model.nv)
+        low_vel = -3 * np.ones(self.model.nv)
+        high_vel = 3 * np.ones(self.model.nv)
 
-        low = .1*np.concatenate((low_pos, low_vel))
-        high = .1*np.concatenate((high_pos, high_vel))
+        low = .1 * np.concatenate((low_pos, low_vel))
+        high = .1 * np.concatenate((high_pos, high_vel))
         self.action_space = spaces.Box(high, low, dtype=np.float32)
         self.observation_space = spaces.Box(high, low, dtype=np.float32)
 
         # Overwrite the action cost.
         self.state_des = np.zeros(14)
-        self.Q = 1e-2*np.eye(14)
-        self.R = 1e-6*np.eye(14)
+        self.Q = 1e-2 * np.eye(14)
+        self.R = 1e-6 * np.eye(14)
         self.eps = 1e-1
 
     def subtree_mass(self):
-        body_names = ['kuka_link_{}'.format(i+1) for i in range(7)]
+        body_names = ['kuka_link_{}'.format(i + 1) for i in range(7)]
         body_ids = [self.model.body_name2id(n) for n in body_names]
         return self.model.body_subtreemass[body_ids]
 
@@ -77,14 +76,14 @@ class IdControlledKukaEnv(kuka_env.KukaEnv):
             self.qpos_set = np.zeros(self.model.nq)
             self.qvel_set = np.zeros(self.model.nv)
             return
-        
+
         # Check if we are setting the action space or small differences
         # from the action space.
         if self.setpoint_diff:
             # Scale to encourage only small differences from the current
             # setpoint
-            self.qpos_set = self.sim.data.qpos + 1e-2*a[:7]
-            self.qvel_set = self.sim.data.qvel + 1e-2*a[7:14]
+            self.qpos_set = self.sim.data.qpos + 1e-2 * a[:7]
+            self.qvel_set = self.sim.data.qvel + 1e-2 * a[7:14]
             self.qvel_set = np.zeros(7)
         else:
             # Set the PD setpoint directly.
@@ -100,16 +99,18 @@ class IdControlledKukaEnv(kuka_env.KukaEnv):
         qvel_err = self.qvel_set - self.sim.data.qvel
 
         # Compute desired acceleration using inner loop PD law
-        self.sim.data.qacc[:] = self.kp_id*qpos_err + self.kd_id*qvel_err
-        mujoco_py.functions.mj_inverse(self.model,self.sim.data)
-        id_torque = self.sim.data.qfrc_inverse[:]/300
+        self.sim.data.qacc[:] = self.kp_id * qpos_err + self.kd_id * qvel_err
+        mujoco_py.functions.mj_inverse(self.model, self.sim.data)
+        id_torque = self.sim.data.qfrc_inverse[:] / 300
 
-        import pdb; pdb.set_trace()
+        import pdb
+        pdb.set_trace()
         # Compute torque from outer loop PD law
-        pd_torque = self.kp_pd*qpos_err + self.kd_pd*qvel_err
+        pd_torque = self.kp_pd * qpos_err + self.kd_pd * qvel_err
 
         # Sum the torques
         return id_torque + pd_torque
+
 
 class DiffIdControlledKukaEnv(IdControlledKukaEnv):
     setpoint_diff = False
