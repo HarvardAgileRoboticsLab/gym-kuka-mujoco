60dc534014f6c97d59d263453a25b1af9e837e2a
John <keszler@college.harvard.edu>
2019-02-15 15:30:35 -0500

    added ID hammer changes

--- gym_kuka_mujoco/controllers/inverse_dynamics_controller.py
+++ gym_kuka_mujoco/controllers/inverse_dynamics_controller.py
@@ -18,16 +18,30 @@ class InverseDynamicsController(BaseController):
                  action_scale=1.,
                  action_limit=1.,
                  kp_id=100.,
-                 kd_id='auto'):
+                 kd_id='auto',
+                 controlled_joints=None):
         super(InverseDynamicsController, self).__init__(sim)
         
         # Create a model for control
         model_path = os.path.join(kuka_asset_dir(), model_path)
         self.model = mujoco_py.load_model_from_path(model_path)
 
+        # Hammer-specific problems
+        self.controlled_joints = controlled_joints
+        if self.controlled_joints is not None:
+            self.controlled_joints = [self.model.joint_name2id(joint) for joint in self.controlled_joints]
+ 
         # Construct the action space.
-        low = self.model.jnt_range[:, 0]*action_limit
-        high = self.model.jnt_range[:, 1]*action_limit
+        # import pdb
+        low = -3*np.ones(self.model.nu)* action_limit
+        high = 3*np.ones(self.model.nu)* action_limit
+        # if not self.controlled_joints:
+        #     low = self.model.jnt_range[:, 0]*action_limit
+        #     high = self.model.jnt_range[:, 1]*action_limit
+        # else:
+        #     low = self.model.jnt_range[self.controlled_joints, 0]*action_limit
+        #     high = self.model.jnt_range[self.controlled_joints, 1]*action_limit
+        # pdb.set_trace()
         self.action_space = spaces.Box(low, high, dtype=np.float32)
         
         # Controller parameters.
@@ -39,14 +53,18 @@ class InverseDynamicsController(BaseController):
             self.kd_id = kd_id
 
         # Initialize setpoint.
-        self.qpos_set = np.zeros(7)
-        self.qvel_set = np.zeros(7)
- 
+        self.qpos_set = np.zeros(self.model.nq)
+        self.qvel_set = np.zeros(self.model.nq)
+
+
     def set_action(self, action):
         '''
         Set the setpoint.
         '''
-        self.qpos_set = self.action_scale * action[:7]
+        if not self.controlled_joints:
+            self.qpos_set = self.action_scale * action[:7]
+        else:
+            self.qpos_set[self.controlled_joints] = self.action_scale * action[:7]
 
     def get_torque(self):
         '''
@@ -57,9 +75,16 @@ class InverseDynamicsController(BaseController):
         qvel_err = self.qvel_set - self.sim.data.qvel
 
         # Compute desired acceleration using inner loop PD law.
-        self.sim.data.qacc[:] = self.kp_id * qpos_err + self.kd_id * qvel_err
+        if not self.controlled_joints:
+            self.sim.data.qacc[:] = self.kp_id * qpos_err + self.kd_id * qvel_err
+        else:
+            self.sim.data.qacc[self.controlled_joints] = self.kp_id * qpos_err[self.controlled_joints] + self.kd_id * qvel_err[self.controlled_joints]
         mujoco_py.functions.mj_inverse(self.model, self.sim.data)
-        id_torque = self.sim.data.qfrc_inverse[:].copy()
+        
+        if not self.controlled_joints:
+            id_torque = self.sim.data.qfrc_inverse[:].copy()
+        else:
+            id_torque = self.sim.data.qfrc_inverse[self.controlled_joints].copy()
 
         # Sum the torques.
         return id_torque
@@ -67,7 +92,10 @@ class InverseDynamicsController(BaseController):
 class RelativeInverseDynamicsController(InverseDynamicsController):
     def set_action(self, action):
         # Set the setpoint difference from the current position.
-        self.qpos_set = self.sim.data.qpos + action[:7]
+        if not self.controlled_joints:
+            self.qpos_set = self.sim.data.qpos + action[:7]
+        else:
+            self.qpos_set[self.controlled_joints] = self.sim.data.qpos[self.controlled_joints] + action[:7]
 
 register_controller(InverseDynamicsController, 'InverseDynamicsController')
 register_controller(RelativeInverseDynamicsController, 'RelativeInverseDynamicsController')
