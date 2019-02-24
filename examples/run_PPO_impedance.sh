e4b4db81c1efcb0ed08d0fd28cd45117c9cbe9ad
Patrick Varin <patrickjvarin@gmail.com>
2019-02-24 14:38:56 -0500

    renaming script to run insertion envs

--- examples/run_PPO_impedance.sh
+++
@@ -1,4 +0,0 @@
-python learn_environment.py --param_file=working/IDPegInsertion:PPO2.json &
-python learn_environment.py --param_file=working/PDPegInsertion:PPO2.json &
-python learn_environment.py --param_file=working/ImpedanceV2PegInsertion:PPO2.json &
-python learn_environment.py --param_file=working/TorquePegInsertion:PPO2.json
