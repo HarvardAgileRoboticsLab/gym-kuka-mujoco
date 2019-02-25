553d1017acd67d0f79cd8830533e425cf47975ed
Patrick Varin <patrickjvarin@gmail.com>
2019-02-25 18:27:41 -0500

    param_files and new run script

---
+++ examples/run_PPO_insertion_final.sh
@@ -0,0 +1,4 @@
+python learn_environment.py --param_file=working/IDPegInsertion:PPO2.json --final &
+python learn_environment.py --param_file=working/PDPegInsertion:PPO2.json --final &
+python learn_environment.py --param_file=working/ImpedanceV2PegInsertion:PPO2.json --final &
+python learn_environment.py --param_file=working/TorquePegInsertion:PPO2.json --final
