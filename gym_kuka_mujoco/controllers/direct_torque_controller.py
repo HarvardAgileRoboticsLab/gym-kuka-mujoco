d699a094991bf87b63d756a12161229fc74b67a9
Patrick Varin <patrickjvarin@gmail.com>
2019-02-26 18:04:51 -0500

    adding gravity comp to PD and Torque controllers

--- gym_kuka_mujoco/controllers/direct_torque_controller.py
+++ gym_kuka_mujoco/controllers/direct_torque_controller.py
@@ -1,7 +1,11 @@
+import os
+
 import numpy as np
 import gym
+import mujoco_py
 
-from gym_kuka_mujoco.utils.mujoco_utils import kuka_subtree_mass
+from gym_kuka_mujoco.envs.assets import kuka_asset_dir
+from gym_kuka_mujoco.utils.mujoco_utils import kuka_subtree_mass, get_qpos_indices, get_qvel_indices, get_actuator_indices, get_joint_indices 
 from .base_controller import BaseController
 from . import register_controller
 
@@ -9,8 +13,41 @@ class DirectTorqueController(BaseController):
     '''
     A simple controller that takes raw torque actions.
     '''
-    def __init__(self, sim, action_scaling=10.):
+    def __init__(self,
+                 sim,
+                 action_scaling=10.,
+                 gravity_comp_model_path=None,
+                 controlled_joints=None):
         super(DirectTorqueController, self).__init__(sim)
+
+        self.gravity_comp = False
+        if gravity_comp_model_path is not None:
+            self.gravity_comp = True
+            model_path = os.path.join(kuka_asset_dir(), gravity_comp_model_path)
+            self.model = mujoco_py.load_model_from_path(model_path)
+            self.gravity_comp_sim = mujoco_py.MjSim(self.model)
+            assert self.model.nv == self.sim.model.nv, \
+                "the model for control and simulation must have the same number of DOF"
+                # Get the position, velocity, and actuator indices for the model.
+            if controlled_joints is not None:
+                self.sim_qpos_idx = get_qpos_indices(sim.model, controlled_joints)
+                self.sim_qvel_idx = get_qvel_indices(sim.model, controlled_joints)
+                self.sim_actuators_idx = get_actuator_indices(sim.model, controlled_joints)
+                self.sim_joint_idx = get_joint_indices(sim.model, controlled_joints)
+
+                self.self_qpos_idx = get_qpos_indices(self.model, controlled_joints)
+                self.self_qvel_idx = get_qvel_indices(self.model, controlled_joints)
+                self.self_actuators_idx = get_actuator_indices(self.model, controlled_joints)
+            else:
+                assert self.model.nv == self.model.nu, "if the number of degrees of freedom is different than the number of actuators you must specify the controlled_joints"
+                self.sim_qpos_idx = range(self.model.nq)
+                self.sim_qvel_idx = range(self.model.nv)
+                self.sim_actuators_idx = range(self.model.nu)
+                self.sim_joint_idx = range(self.model.nu)
+
+                self.self_qpos_idx = range(self.model.nq)
+                self.self_qvel_idx = range(self.model.nv)
+                self.self_actuators_idx = range(self.model.nu)
 
         # Scale the actions proportionally to the subtree mass.
         true_subtree_mass = kuka_subtree_mass(sim.model)
@@ -26,7 +63,17 @@ class DirectTorqueController(BaseController):
         self.torque = action*self.action_scaling
 
     def get_torque(self):
-        return self.torque
+        torque = self.torque.copy()
+        
+        # Add gravity compensation if necessary
+        if self.gravity_comp:
+            self.gravity_comp_sim.data.qpos[:] = self.sim.data.qpos[:].copy()
+            self.gravity_comp_sim.data.qvel[:] = np.zeros(self.model.nv)
+            self.gravity_comp_sim.data.qacc[:] = np.zeros(self.model.nv)
+            mujoco_py.functions.mj_inverse(self.model, self.gravity_comp_sim.data)
+            torque += self.gravity_comp_sim.data.qfrc_inverse[self.sim_actuators_idx].copy()
+
+        return torque
 
 class SACTorqueController(DirectTorqueController):
     '''
