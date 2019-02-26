a9acca681bf9a55d5f6d371ad980f447449d215b
Patrick Varin <patrickjvarin@gmail.com>
2019-02-26 14:24:24 -0500

    increasing stepsize in sim

---
+++ examples/run_PPO_hammer.sh
@@ -0,0 +1,4 @@
+python learn_environment.py --param_file=working/hammer/IDHammer:PPO2.json &
+# python learn_environment.py --param_file=working/hammer/PDHammer:PPO2.json &
+python learn_environment.py --param_file=working/hammer/ImpedanceV2Hammer:PPO2.json &
+# python learn_environment.py --param_file=working/hammer/TorqueHammer:PPO2.json
