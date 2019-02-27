d6dc4af9334b391889c014da2f1a22820ca910c6
Patrick Varin <patrickjvarin@gmail.com>
2019-02-27 00:33:52 -0500

    adding minimal obs to run script

--- examples/run_PPO_insertion.sh
+++ examples/run_PPO_insertion.sh
@@ -1,4 +1,5 @@
 python learn_environment.py --param_file=working/peg_insertion/IDPegInsertion:PPO2.json &
-# python learn_environment.py --param_file=working/peg_insertion/PDPegInsertion:PPO2.json &
+python learn_environment.py --param_file=working/peg_insertion/PDPegInsertion:PPO2.json &
 python learn_environment.py --param_file=working/peg_insertion/ImpedanceV2PegInsertion:PPO2.json &
-# python learn_environment.py --param_file=working/peg_insertion/TorquePegInsertion:PPO2.json
+python learn_environment.py --param_file=working/peg_insertion/ImpedanceV2PegInsertionMinimalObs:PPO2.json &
+python learn_environment.py --param_file=working/peg_insertion/TorquePegInsertion:PPO2.json
