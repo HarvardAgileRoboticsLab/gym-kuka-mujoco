01d858bb4f1995d4642816800cc4ba60dcfc89bb
Patrick Varin <pvarin@users.noreply.github.com>
2019-04-02 15:50:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt')
-
-# reqs is a list of requirement
-reqs = [str(ir.req) for ir in install_reqs]
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(name='gym_kuka_mujoco',
       version='0.0.1',
-      install_requires=reqs,  # And any other dependencies foo needs
+      install_requires=required,  # And any other dependencies foo needs
       packages=find_packages()
 )
