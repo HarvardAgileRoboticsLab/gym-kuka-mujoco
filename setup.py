f706c6bd923c799d14f03f4859c63a024584d5ba
Patrick Varin <pvarin@users.noreply.github.com>
2019-04-02 15:42:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(name='gym_kuka_mujoco',
       version='0.0.1',
       install_requires=[
-        'gym[mujoco]'
+        'gym[mujoco]',
+        'commentjson'
       ],  # And any other dependencies foo needs
       packages=find_packages()
 )
