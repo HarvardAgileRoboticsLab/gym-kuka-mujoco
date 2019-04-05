06e5644be5a9d52c247a69c5e7be1cf6061dd54f
Patrick Varin <pvarin@users.noreply.github.com>
2019-04-05 15:11:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,12 @@ setup(name='gym_kuka_mujoco',
       packages=find_packages(),
       package_data={
           'gym_kuka_mujoco.envs.assets': '*.xml',
+          'gym_kuka_mujoco.envs.assets.kuka': '*.xml'
           'gym_kuka_mujoco.envs.assets.peg': '*.xml'
+          'gym_kuka_mujoco.envs.assets.meshes': '*.stl'
+          'gym_kuka_mujoco.envs.assets.hammer': '*.xml'
+          'gym_kuka_mujoco.envs.assets.hole': '*.xml'
+          'gym_kuka_mujoco.envs.assets.pushing': '*.xml'
+          'gym_kuka_mujoco.envs.assets.valve': '*.xml'
       }
 )
