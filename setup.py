d5c93187dfb771fb7d6de19a0ab332ba7d5d7c16
Patrick Varin <pvarin@users.noreply.github.com>
2019-04-05 15:04:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,12 @@ from glob import glob
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-def get_files_recursive(root, ext='.xml'):
-    matches = []
-    for path, dirs, files in os.walk(root):
-        matches.extend([os.path.join(path, f) for f in files if ext in f])
-    return matches
-    
-    
-print(get_files_recursive('./gym_kuka_env/envs/assets', ext='.xml'))
-
 setup(name='gym_kuka_mujoco',
       version='0.0.1',
       install_requires=required,
       packages=find_packages(),
-      package_data={'': glob('./gym_kuka_env/envs/assets/*.xml')
-                      + glob('./gym_kuka_env/envs/assets/*/*.xml')}
+      package_data={
+          'gym_kuka_mujoco.envs.assets': '*.xml',
+          'gym_kuka_mujoco.envs.assets.peg': '*.xml'
+      }
 )
