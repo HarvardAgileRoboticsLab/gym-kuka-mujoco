ddcc17d6ed0fa7ba19aee4e905d7661e58c377d5
Patrick Varin <pvarin@users.noreply.github.com>
2019-04-05 15:01:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(name='gym_kuka_mujoco',
       version='0.0.1',
       install_requires=required,
       packages=find_packages(),
-      package_data={'': glob('./gym_kuka_env/envs/asssets/*.xml')
-                      + glob('./gym_kuka_env/envs/asssets/peg/*.xml')}
+      package_data={'': glob('./gym_kuka_env/envs/assets/*.xml')
+                      + glob('./gym_kuka_env/envs/assets/*/*.xml')}
 )
